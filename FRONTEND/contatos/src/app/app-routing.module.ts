6ae3d0e752730eefd87ed48c4ce9000ca3e78a93
Marcos Dessa (UCSal) <marcosdessa.oliveira@ucsal.edu.br>
2023-09-01 18:10:24 -0300

    FRONT: primeiro esboço dos components

--- FRONTEND/contatos/src/app/app-routing.module.ts
+++ FRONTEND/contatos/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { LinksComponent } from './components/links/links.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent, children: [
+      { path: 'sobre', component: SobreComponent },
+      { path: 'contatos', component: ContatosComponent },
+      { path: 'links', component: LinksComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FRONTEND/contatos/src/app/app.module.ts
+++ FRONTEND/contatos/src/app/app.module.ts
@@ -4,15 +4,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { LinksComponent } from './components/links/links.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    ContatosComponent,
+    LinksComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // Material
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatListModule,
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
